6fd6a7569d42961fab75be7a082f4575eeee64c5
Bruce MBP <s.hu@queensu.ca>
2023-08-19 18:26:09 -0400

    integrate json formatter

---
+++ gpt_utils.py
@@ -0,0 +1,54 @@
+import os
+import openai
+
+default_prompt = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Provide a recipe for spaghetti bolognese"}
+  ]
+
+default_schema = {
+  "type": "object",
+  "properties": {
+    "ingredients": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "name": { "type": "string" },
+          "unit": { 
+            "type": "string",
+            "enum": ["grams", "ml", "cups", "pieces", "teaspoons"]
+          },
+          "amount": { "type": "number" }
+        },
+        "required": ["name", "unit", "amount"]
+      }
+    },
+    "instructions": {
+      "type": "array",
+      "description": "Steps to prepare the recipe (no numbering)",
+      "items": { "type": "string" }
+    },
+    "time_to_cook": {
+      "type": "number",
+      "description": "Total time to prepare the recipe in minutes"
+    }
+  },
+  "required": ["ingredients", "instructions", "time_to_cook"]
+}
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def get_list(schema=default_schema, prompt=default_prompt, temperature=0):
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0613",
+    messages=prompt,
+    functions=[{"name": "own_function", "parameters": schema}],
+    function_call={"name": "own_function"},
+    temperature=temperature,
+    )
+
+    return completion.choices[0].message.function_call.arguments
+
